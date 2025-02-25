19be42cccb2554d11474522453409f036612deb9
Szymon Domżalski <100307341+szdomzalski@users.noreply.github.com>
2025-02-25 22:07:51 +0100

    Testing signing serialized code

--- sign_code.py
+++ sign_code.py
@@ -1,5 +1,6 @@
 import hashlib
 import hmac
+from pathlib import Path
 import dill
 from typing import Any, BinaryIO, Callable
 
@@ -16,3 +17,26 @@ def safe_load(file: BinaryIO, key: str, algorithm: Callable = hashlib.sha256) ->
     if signature != hmac.new(key.encode(), serialized_data, algorithm).digest():
         raise dill.UnpicklingError("Signature mismatch")
     return dill.loads(serialized_data)
+
+
+if __name__ == "__main__":
+    path = Path("code.pkl")
+    code = lambda x, y: x + y
+
+    with path.open("wb") as file:
+        safe_dump(code, file, "my_secret_key")
+        print(code)
+        print(code(1, 2))
+
+    with path.open("rb") as file:
+        loaded_code = safe_load(file, "my_secret_key")
+        print(loaded_code)
+        print(loaded_code(1, 2))
+        print(loaded_code == code)
+        print(id(loaded_code) == id(code))
+
+    with path.open("rb") as file:
+        try:
+            safe_load(file, "wrong_key")
+        except dill.UnpicklingError as e:
+            print(e)
